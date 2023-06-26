109280633c9c8ee3d35b4a63e325541cc5683b2d
InnovateHer <111238442+innovateherr@users.noreply.github.com>
2023-06-26 21:59:36 +0530

    Add files via upload

---
+++ src/components/Hero.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Hero = () => {
+  return (
+    <div className='text-white'>
+      <div className='max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center'>
+        <p className='text-[#00df9a] font-bold p-2'>
+          GROWING WITH DATA ANALYTICS
+        </p>
+        <h1 className='md:text-7xl sm:text-6xl text-4xl font-bold md:py-6'>
+          Grow with data.
+        </h1>
+        
+        <p className='md:text-2xl text-xl font-bold text-gray-500'>Monitor your data analytics to increase revenue for BTB, BTC, & SASS platforms.</p>
+        <button className='bg-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto py-3 text-black'>Get Started</button>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
